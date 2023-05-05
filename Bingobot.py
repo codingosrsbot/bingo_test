3a32189dbd29ca1fd9adc41dd8869021f9ac8288
codingosrsbot <codingbot73@outlook.be>
2023-05-06 00:23:31 +0200

    V0.1

---
+++ Bingobot.py
@@ -0,0 +1,62 @@
+
+import discord #pip install discord.py
+from discord.ext import commands
+import asyncio
+import logging
+import os.path
+
+
+import WOM
+import BingoBoard
+
+
+# Settings
+
+with open("Token.txt", 'r') as fp:
+    gTOKEN = fp.readline()
+    
+# Bot
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+description = '''Discord osrs bot'''
+bot = commands.Bot(intents=intents, command_prefix= "<" , description='Bingo time',  case_insensitive=True)
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def woodcutmvp(ctx: discord.ext.commands.Context):
+	id = 24073
+	teamName = "special people"
+	skill = "woodcutting"
+	BingoComp = WOM.WOMcomp(id)
+	BingoComp.getWOMData(skill)
+	BingoComp.getWOMTeamData(BingoComp.DataTeamLists, teamName)
+	print(BingoComp.teamdata.teamName + " asked " + skill +" mvp")
+	await ctx.send("The " + skill  + " mvp of " + BingoComp.teamdata.teamName + " is ... " + BingoComp.teamdata.MVP)
+	
+@bot.command()
+async def woodcutxp(ctx: discord.ext.commands.Context):
+	id = 24073
+	teamName = "special people"
+	skill = "woodcutting"
+	BingoComp = WOM.WOMcomp(id)
+	BingoComp.getWOMData(skill)
+	BingoComp.getWOMTeamData(BingoComp.DataTeamLists, teamName)
+	print(BingoComp.teamdata.teamName + " asked " + skill +" xp")
+	await ctx.send("The " + skill  + " xp of " + BingoComp.teamdata.teamName + " is ... " + BingoComp.teamdata.TotalXP)
+        
+@bot.command()
+async def board(ctx: discord.ext.commands.Context):
+	await BingoBoard.fillBoard(ctx)
+	
+logging.basicConfig(level=logging.DEBUG)
+
+bot.run(gTOKEN)
